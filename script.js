b417bb3468f1e0bf1acd9aed0f507d5c600bb5e0
88rcc4w9gg-collab <88rcc4w9gg@privaterelay.appleid.com>
2026-07-10 06:31:28 +0300

    Update script.js

--- script.js
+++ script.js
@@ -56,5 +56,33 @@ button.addEventListener("mouseenter", () => {
 button.addEventListener("mouseleave", () => {
 
     button.style.transform = "scale(1)";
+// زر الرجوع للأعلى
 
+const topBtn = document.getElementById("topBtn");
+
+window.onscroll = function(){
+
+if(document.body.scrollTop>200 || document.documentElement.scrollTop>200){
+
+topBtn.style.display="block";
+
+}else{
+
+topBtn.style.display="none";
+
+}
+
+}
+
+function topFunction(){
+
+window.scrollTo({
+
+top:0,
+
+behavior:"smooth"
+
+});
+
+}
 });
